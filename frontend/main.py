9a0e9ae5e88979693d4783feb0d3ac75386487bf
Yassine EL KHATTABI <yelkhattabi@scortex.io>
2022-02-25 00:31:11 +0100

    ad main frontend file

---
+++ frontend/main.py
@@ -0,0 +1,42 @@
+import requests
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(page_title='Urban Sematic Segmentation',page_icon="cityscape", layout = 'wide', initial_sidebar_state = 'auto')
+
+# defines an h1 header
+st.title("Urban Semantic Segmentation :cityscape: :car: :motorway: 🌳 🧍")
+
+# displays a file uploader widget
+
+
+image = st.sidebar.file_uploader("Chose an image file to upload",type= ['png', 'jpg','jpeg','WebP','bmp','tiff'] )
+res = None
+
+
+if image is not None:
+    st.header("input image")
+    st.image(image,use_column_width=True)
+    
+else:
+    st.header("Your image will be displayed here once you upload it")
+
+if st.sidebar.button("Predict",help="Click on this button to run segmentation model on the uploaded image"):
+    if image is not None :
+        files = {"file": image.getvalue()}
+        res = requests.post(f"http://127.0.0.1:8000/predict",files=files)
+        paths = res.json()
+else : 
+    st.header("Segmented image will be displayed here")
+
+if res is not None:
+    overlay = Image.open(paths.get("overlay_name"))
+    st.header("Image with segmentaiton mask")
+    st.image(overlay,use_column_width=True)
+    mask = Image.open(paths.get("mask_name"))
+    st.header("Segmentation mask")
+    st.image(mask,use_column_width=True)
+    st.sidebar.header("Mask colors legends")
+    legend = Image.open(paths.get("legend_name"))
+    st.sidebar.image(legend,width=200)
+        
